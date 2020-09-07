c5e25fec644f2b6f65a9063566aaf8b43b9136cf
Kosenko Oleg <silverline170269@gmail.com>
2020-09-07 10:19:50 -0400

    Initial Commit

---
+++ SimpleDictionary/SimpleDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleDictionaryApp.swift
+//  SimpleDictionary
+//
+//  Created by Oleg Kosenko on 2020-09-07.
+//
+
+import SwiftUI
+
+@main
+struct SimpleDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
