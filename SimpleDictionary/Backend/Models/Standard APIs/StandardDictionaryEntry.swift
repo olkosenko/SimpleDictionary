d06603fc45c85ff90c01daa8f46c933df99090c7
Kosenko Oleg <silverline170269@gmail.com>
2020-11-13 13:56:35 -0500

    Dependencies.

---
+++ SimpleDictionary/Backend/Models/Standard APIs/StandardDictionaryEntry.swift
@@ -0,0 +1,19 @@
+//
+//  StandardDictionaryEntry.swift
+//  SimpleDictionary
+//
+//  Created by Oleg Kosenko on 2020-11-12.
+//
+
+import Foundation
+
+struct StandardDictionaryEntry {
+    let word: String
+    let phoneticSpelling: String?
+    // sound
+    let definitions: [String]
+    let synonyms: [String]?
+    let antonyms: [String]?
+    let examples: [String]?
+    let etymologies: [String]?
+}
