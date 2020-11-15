25eda67411a10b5a1eab41ea6d4bed5a0f292602
Kosenko Oleg <silverline170269@gmail.com>
2020-11-15 10:04:26 -0500

    Update OXFRetrieveEntry.swift

--- SimpleDictionary/Backend/Models/Standard APIs/Oxford API/OXFRetrieveEntry.swift
+++ SimpleDictionary/Backend/Models/Standard APIs/Oxford API/OXFRetrieveEntry.swift
@@ -11,25 +11,3 @@ struct OXFRetrieveEntry: Decodable {
     /// A list of entries and all the data related to them.
     let results: [OXFHeadwordEntry]?
 }
-
-extension OXFRetrieveEntry: EntryConvertable {
-    func convert() -> [Entry] {
-        guard let result = self.results else { return [] }
-        let mapped = result
-            .compactMap { $0.lexicalEntries }
-            .flatMap { $0 }
-            .compactMap { $0.entries }
-            .flatMap { $0 }
-            .compactMap { $0.senses }
-            .flatMap { $0 }
-            .compactMap { sense -> Entry? in
-                if let definitions = sense.definitions {
-                    let examples = sense.examples?.map { $0.text }
-                    return Entry(definitions: definitions, examples: examples ?? [])
-                }
-                return nil
-            }
-        
-        return mapped
-    }
-}
